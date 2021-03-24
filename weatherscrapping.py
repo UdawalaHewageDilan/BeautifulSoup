41ba44e906223362bf973e376a79f1d19bfde4d7
Udawala Hewage Dilan <79456800+UdawalaHewageDilan@users.noreply.github.com>
2021-03-24 15:47:30 +0100

    Update weatherscrapping.py
    3 D parsing done

--- weatherscrapping.py
+++ weatherscrapping.py
@@ -6,10 +6,15 @@ import pandas as pd
 source = rq.get("https://forecast.weather.gov/MapClick.php?lat=37.777120000000025&lon=-122.41963999999996#.YFspOK_0nIU").text
 
 soup = BeautifulSoup(source, 'lxml')
-#print(soup.prettify())
 
-today = soup.find()
-print(today)
+
+article = soup.find(id="detailed-forecast-body")
+#
+for item in article.find_all(class_='row row-odd row-forecast'):
+    print(item.div.b.text)
+    print(item.text)
+
+
 
 
 
