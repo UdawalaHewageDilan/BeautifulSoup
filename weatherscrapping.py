de844333c1c39cf54b84664798fad7bc0cf06eb1
Udawala Hewage Dilan <79456800+UdawalaHewageDilan@users.noreply.github.com>
2021-03-24 13:59:05 +0100

    Update weatherscrapping.py
    2 D Start to the project

--- weatherscrapping.py
+++ weatherscrapping.py
@@ -1,2 +1,15 @@
 import requests as rq
 from bs4 import BeautifulSoup
+import pandas as pd
+
+
+source = rq.get("https://forecast.weather.gov/MapClick.php?lat=37.777120000000025&lon=-122.41963999999996#.YFspOK_0nIU").text
+
+soup = BeautifulSoup(source, 'lxml')
+#print(soup.prettify())
+
+today = soup.find()
+print(today)
+
+
+
